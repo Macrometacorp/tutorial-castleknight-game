55f9f04f0388a94469878c0247d0aae949176752
Abhishek <abhishek.lohani@macrometa.co>
2020-10-28 16:51:47 +0530

    updated Config.js for default login

--- js/Config.js
+++ js/Config.js
@@ -1,4 +1,4 @@
-var cluster = "gdn1.prod.macrometa.io";
+var cluster = "qa1-sfo2.eng3.macrometa.io";
 var email = "xxxx@macrometa.io";
-var fabric_name = "_system";
+var fabric_name = "xxxx";
 var password = "xxxx";
